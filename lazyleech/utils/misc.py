f4b25451c04a85eeb1827e0880521b53d2929d57
akane-tsunemori <76539168+akane-tsunemori@users.noreply.github.com>
2021-06-02 08:39:42 +0530

    Update misc.py

--- lazyleech/utils/misc.py
+++ lazyleech/utils/misc.py
@@ -81,7 +81,7 @@ def return_progress_string(current, total):
         filled_length = int(30 * current // total)
     else:
         filled_length = 0
-    return '[' + '=' * filled_length + ' ' * (30 - filled_length) + ']'
+    return '[' + '▰' * filled_length + ' ' * (30 - filled_length) + ']'
 
 # https://stackoverflow.com/a/852718
 # https://stackoverflow.com/a/775095
