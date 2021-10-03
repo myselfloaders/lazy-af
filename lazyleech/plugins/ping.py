10e19e87ad7f17ee01ba5c3b3e19cfd41060e412
akane-tsunemori <76539168+akane-tsunemori@users.noreply.github.com>
2021-10-03 19:23:16 +0530

    Update ping.py

--- lazyleech/plugins/ping.py
+++ lazyleech/plugins/ping.py
@@ -3,4 +3,4 @@ from .. import ALL_CHATS
 
 @Client.on_message(filters.command('ping') & filters.chat(ALL_CHATS))
 async def ping_pong(client, message):
-    await message.reply_text('I Am Breating Onii-chan ^▽^. \nThank You For Asking {mention}')
+    await message.reply_text('I Am Breating Onii-chan ^▽^. \nThank You For Asking')
