b15222db63f62b404577d8fc249d5298f631c7e5
akane-tsunemori <76539168+akane-tsunemori@users.noreply.github.com>
2021-10-03 19:09:10 +0530

    Update autodetect.py

--- lazyleech/plugins/autodetect.py
+++ lazyleech/plugins/autodetect.py
@@ -42,7 +42,7 @@ async def autodetect(client, message):
                 link = text
     if link:
         reply = await message.reply_text(f'{"Torrent" if is_torrent else "Magnet"} detected. Select upload method', reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton('Individual Files', 'autodetect_individual'), InlineKeyboardButton('Zip', 'autodetect_zip'), InlineKeyboardButton('Force Document', 'autodetect_file')],
+            [InlineKeyboardButton('Streamable', 'autodetect_individual'), InlineKeyboardButton('Zip', 'autodetect_zip'), InlineKeyboardButton('Document', 'autodetect_file')],
             [InlineKeyboardButton('Delete', 'autodetect_delete')]
         ]))
         auto_detects[(reply.chat.id, reply.message_id)] = link, message.from_user.id, (initiate_torrent if is_torrent else initiate_magnet)
