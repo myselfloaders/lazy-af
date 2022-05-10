0b046a934aeedc68a33d2a6ea0981d20a7c92d68
Tera Baap Hu <91613526+myselfloaders@users.noreply.github.com>
2022-05-10 15:33:25 +0530

    Update ping.py

--- lazyleech/plugins/ping.py
+++ lazyleech/plugins/ping.py
@@ -3,4 +3,4 @@ from .. import ALL_CHATS
 
 @Client.on_message(filters.command('ping') & filters.chat(ALL_CHATS))
 async def ping_pong(client, message):
-    await message.reply_text('I Am Breating Onii-chan ^▽^. \nThank You For Checking on Me. \n❤️Love You❤️')
+    await message.reply_text('I Am Still Alive')
